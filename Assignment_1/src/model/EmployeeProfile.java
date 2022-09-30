fcc26c3604b9a75100e04c70468bd7600078312e
shu3hamiitkgp <goyal.shu@northeastern.edu>
2022-09-30 13:52:06 -0400

    first commit

---
+++ Assignment_1/src/model/EmployeeProfile.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author shubhamgoyal
+ */
+public class EmployeeProfile {
+    
+    private String Name;
+    private String EmployeeID;
+    private int Age;
+    privat String Gender;
+    private String StartDate;
+    private String Level;
+    private String TeamInfo;
+    private String PositionTitle;
+    private String CellPhoneNumber;
+    private String EmailAddress;
+    private String PhotoPath;
+
+}
